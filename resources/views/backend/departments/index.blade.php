c32f5d8f1642877e0f3f9c606dc25a64990e4ee8
alex <alexphuyal175@gmail.com>
2025-04-01 15:19:23 +0545

    final of department

--- resources/views/backend/departments/index.blade.php
+++ resources/views/backend/departments/index.blade.php
@@ -18,12 +18,7 @@
                 </div>
             </div>
             <div class="block-content">
-                @if (session('success'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        {{ session('success') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
+                
 
                 <div class="table-responsive">
                     <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
--- resources/views/backend/departments/show.blade.php
+++ resources/views/backend/departments/show.blade.php
@@ -20,36 +20,52 @@
             </div>
             <div class="block-content">
                 <div class="row">
-                    <div class="col-lg-12">
-                        <div class="table-responsive">
-                            <table class="table table-bordered">
-                                <tbody>
-                                    <tr>
-                                        <th class="text-center" style="width: 200px;">ID</th>
-                                        <td>{{ $department->id }}</td>
-                                        <th class="text-center" style="width: 200px;">Name</th>
-                                        <td>{{ $department->name }}</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-center">Display Order</th>
-                                        <td>{{ $department->display_order }}</td>
-                                        <th class="text-center">Status</th>
-                                        <td class="text-center">
-                                            @if ($department->is_published)
-                                                <span class="badge bg-success">Published</span>
-                                            @else
-                                                <span class="badge bg-warning">Draft</span>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-center">Created</th>
-                                        <td>{{ $department->created_at->format('M d, Y H:i') }}</td>
-                                        <th class="text-center">Last Updated</th>
-                                        <td>{{ $department->updated_at->format('M d, Y H:i') }}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="block block-rounded">
+                            <div class="block-header block-header-default">
+                                <h3 class="block-title">Basic Information</h3>
+                            </div>
+                            <div class="block-content">
+                                <div class="row mb-2">
+                                    <div class="col-md-4 fw-semibold text-muted">ID:</div>
+                                    <div class="col-md-8">{{ $department->id }}</div>
+                                </div>
+                                <div class="row mb-2">
+                                    <div class="col-md-4 fw-semibold text-muted">Name:</div>
+                                    <div class="col-md-8">{{ $department->name }}</div>
+                                </div>
+                                <div class="row mb-2">
+                                    <div class="col-md-4 fw-semibold text-muted">Display Order:</div>
+                                    <div class="col-md-8">{{ $department->display_order }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="block block-rounded">
+                            <div class="block-header block-header-default">
+                                <h3 class="block-title">Additional Details</h3>
+                            </div>
+                            <div class="block-content">
+                                <div class="row mb-2">
+                                    <div class="col-md-4 fw-semibold text-muted">Status:</div>
+                                    <div class="col-md-8">
+                                        @if ($department->is_published)
+                                            <span class="badge bg-success">Published</span>
+                                        @else
+                                            <span class="badge bg-warning">Draft</span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="row mb-2">
+                                    <div class="col-md-4 fw-semibold text-muted">Created At:</div>
+                                    <div class="col-md-8">{{ $department->created_at->format('M d, Y H:i') }}</div>
+                                </div>
+                                <div class="row mb-2">
+                                    <div class="col-md-4 fw-semibold text-muted">Updated At:</div>
+                                    <div class="col-md-8">{{ $department->updated_at->format('M d, Y H:i') }}</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>

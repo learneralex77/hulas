d1cce10b80d8a2632f35114e5dce6cb32efe04fc
Birajwagley <birazwagley8739@gmail.com>
2025-04-04 12:14:32 +0545

    Added in the App ServiceProvider

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -6,19 +6,30 @@ use Illuminate\Http\Request;
 use App\Models\Designation;
 use App\Models\Team;
 use App\Models\AboutUs;
+use App\Models\Gallery;
+use App\Models\Page;
+use App\Models\Publication;
+use App\Models\Service;
 
 class FrontendController extends Controller
 {
     public function homepage()
     {
-        // $aboutUs = AboutUs::active()->orderBy('display_order', 'ASC')->first();
+        // $aboutUs = AboutUs::first();
+        // $popup = Popup::active()->orderBy('display_order', 'ASC')->get();
+        // $popupPaths = $popup->pluck('photo')->map(fn($path) => asset('storage' . $path));
+        // $howToBecameAnAgent = Page::where('slug', 'how-become-an-agent')->first();
+        // $sliders = Slider::active()->orderBy('display_order', 'ASC')->get();
+        // $services = Service::active()->orderBy('display_order', 'ASC')->get();
+        // $notices = Publication::active()->where('publication_type', 'notice')->orderBy('display_order', 'ASC')->get();
+        // $galleries = Gallery::active()->where('is_published', 1)->take(9)->latest()->get();
+        // return view('frontend.homepage', compact('aboutUs', 'popup', 'popupPaths', 'howToBecameAnAgent', 'sliders', 'services', 'notices', 'galleries',));
 
         return view('frontend.homepage');
     }
 
     public function aboutUs()
     {
-
         return view('frontend.about-us-page');
     }
     public function becomeAnAgent()

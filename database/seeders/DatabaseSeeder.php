704186cd50411e470edfb35d55003f5d6475ad47
Birajwagley <birazwagley8739@gmail.com>
2025-04-07 11:42:42 +0545

    Added the Seeder of Zone and district

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserTableSeeder::class,
-            
+            ZoneTableSeeder::class,
+            DistrictTableSeeder::class,
+
         ]);
     }
 }

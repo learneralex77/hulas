145250c63d05e70da4e6d8f53b8a21860f810d21
alex <alexphuyal175@gmail.com>
2025-04-11 22:36:28 +0545

    gallery updated

--- app/Http/Requests/GalleryRequest.php
+++ app/Http/Requests/GalleryRequest.php
@@ -25,7 +25,7 @@ class GalleryRequest extends FormRequest
         return [
             'title_en' => ['required', 'string', 'max:255'],
             'title_np' => ['nullable','string', 'max:255'],
-
+            'short_description' => ['nullable', 'string'],
             'featured_image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,webp', 'max:5120'],
             'gallery_images.*' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,webp', 'max:5120'],
             'links' => ['nullable', 'string'],
@@ -48,8 +48,7 @@ class GalleryRequest extends FormRequest
         return [
             'title_en' => 'gallery title(English)',
             'title_np' => 'gallery title(Nepali)',
-
-
+            'short_description' => 'short description',
             'featured_image' => 'featured image',
             'gallery_images.*' => 'gallery image',
             'links' => 'links',
@@ -74,6 +73,7 @@ class GalleryRequest extends FormRequest
             'title_np.string' => 'The gallery title must be a string.',
             'title_np.max' => 'The gallery title may not be greater than 255 characters.',
 
+            'short_description.string' => 'The short description must be a string.',
 
             'featured_image.image' => 'The featured image must be an image file.',
             'featured_image.mimes' => 'The featured image must be a file of type: jpeg, png, jpg, gif, webp.',

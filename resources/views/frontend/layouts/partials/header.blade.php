96c8694eefe2eba9a11e6f56bb6f6bfc0e928703
Birajwagley <birazwagley8739@gmail.com>
2025-04-13 15:50:48 +0545

    Sticky Header

--- resources/views/frontend/layouts/partials/header.blade.php
+++ resources/views/frontend/layouts/partials/header.blade.php
@@ -1,159 +1,128 @@
-<header>
-  <div class="bg-white">
-    <!-- Top Nav -->
-    <nav class="hidden w-full py-2 bg-white text-black border-b-2 px-10 lg:block">
-      <div class="w-full flex justify-between items-center">
-        <!-- Contact Information Section -->
-        <div class="flex space-x-8 items-center">
-          <!-- Address -->
-          <div class="flex space-x-3 items-center">
-          <img src="{{ asset('assets/images/navbar/location-icon.png') }}"
-              class="w-5"
-              alt="Location Icon"
-            />
-            <p class="text-xs">
-              Bagdurbar, Sundhara (Near to China Town Gate) Kathmandu, Nepal
-            </p>
-          </div>
-
-          <!-- Phone Numbers -->
-          <div class="flex space-x-3 items-center">
-          <img src="{{ asset('assets/images/navbar/phone-call-icon.png') }}"
-
-              class="w-5"
-              alt="Phone Icon"
-            />
-            <p class="text-xs">
-              +977 1 5361313, 5358225, 5352008, Toll Free Number: 16600 111222
-              (For NTC Users Only)
-            </p>
-          </div>
-
-          <!-- Email Addresses -->
-          <div class="flex space-x-3 items-center">
-            <img
-             src="{{ asset('assets/images/navbar/mail-icon.png') }}"
-            class="w-5"
-              alt="Email Icon"
-            />
-            <p class="text-xs">
-              info@hulasremittance.com, csc@hulasremittance.com
-            </p>
-          </div>
+<header class="sticky -top-0 lg:-top-16 z-50">
+  <!---------- top-nav ---------->
+  <nav class="hidden w-full py-2 bg-bgprimary border-b-2 px-10 lg:block">
+    <div class="w-full p-2 flex justify-between items-center">
+      <!-- address, toll free -->
+      <div class="flex space-x-8 items-center">
+        <div class="flex space-x-3 items-center">
+                   <img src="{{ asset('assets/images/navbar/location-icon.png') }}"
+ class="w-5" alt="" />
+          <p class="text-xs">
+            Bagdurbar, Sundhara (Near to China Town Gate) Kathmandu, Nepal
+          </p>
         </div>
-
-        <!-- Social Media Links -->
-        <div class="flex space-x-5 items-center">
-          <a rel="noopener noreferrer" href="#">
-          <img
+        <div class="flex space-x-3 items-center">
+         <img src="{{ asset('assets/images/navbar/phone-call-icon.png') }}" class="w-5" alt="" />
+          <p class="text-xs">
+            +977 1 5361313, 5358225, 5352008, Toll Free Number: 16600 111222
+            (For NTC Users Only)
+          </p>
+        </div>
+        <div class="flex space-x-3 items-center">
+          <img   src="{{ asset('assets/images/navbar/mail-icon.png') }}" class="w-5" alt="" />
+          <p class="text-xs">
+            info@hulasremittance.com, csc@hulasremittance.com
+          </p>
+        </div>
+      </div>
+      <!-- social -->
+      <div class="flex space-x-5 items-center">
+        <a rel="noopener noreferrer" href="#"
+          ><img
              src="{{ asset('assets/images/navbar/fb-icon.png') }}"
-              class="w-6"
-              alt="Facebook Icon"
-            />
-          </a>
-          <a rel="noopener noreferrer" href="#">
-       <img
-             src="{{ asset('assets/images/navbar/x-icon.png') }}"
-              class="w-4"
-              alt="X (Twitter) Icon"
-            />
-          </a>
-          <a rel="noopener noreferrer" href="#">
-        <img
-             src="{{ asset('assets/images/navbar/linked-in-icon.png') }}"
-              class="w-5"
-              alt="LinkedIn Icon"
-            />
-          </a>
-        </div>
+            class="w-6"
+            alt="facebook Icon"
+        /></a>
+        <a rel="noopener noreferrer" href="#"
+          ><img  src="{{ asset('assets/images/navbar/x-icon.png') }}" class="w-4" alt="X Icon"
+        /></a>
+        <a rel="noopener noreferrer" href="#"
+          ><img
+            src="{{ asset('assets/images/navbar/linked-in-icon.png') }}"
+            class="w-5"
+            alt="Linkedin Icon"
+        /></a>
       </div>
-    </nav>
+    </div>
+  </nav>
+  <!---------- top-nav ---------->
 
-    <!-- Bottom Nav (Sticky) -->
-    <nav class="sticky top-0 z-50 px-4 pr-8 py-2 flex justify-between items-center bg-white text-black shadow-lg">
-      <!-- Company Logo -->
-      <a class="text-xl lg:pl-10 font-bold leading-none" href="index.html">
+  <!---------- bottom-nav ---------->
+  <nav
+    class="relative px-4 pr-8 py-4 flex justify-between items-center bg-white shadow-lg"
+  >
+    <a class="text-xl lg:pl-10 font-bold leading-none" href="index.html">
       <img src="{{ asset('assets/images/logo/hulas-remittance-logo.jpg') }}"
+        class="w-56"
+        alt="Hulas Remittance Logo"
+      />
+    </a>
 
-          class="w-56"
-          alt="Hulas Remittance Logo"
-        />
-      </a>
-
-      <!-- Desktop Navigation Links -->
-      <div class="hidden lg:block">
-        <ul class="flex">
-          <li class="mb-1">
-            <a
-              class="block p-4 text-sm font-semibold text-black hover:text-accent"
-              href="index.html"
-              >Home</a
-            >
-          </li>
-          <li class="mb-1">
-            <a
-              class="block p-4 text-sm font-semibold text-black hover:text-accent"
-              href="about-us.html"
-              >About us</a
-            >
-          </li>
-          <li class="mb-1">
-            <a
-              class="block p-4 text-sm font-semibold text-black hover:text-accent"
-              href="become-an-agent.html"
-              >Become an agent</a
-            >
-          </li>
-          <li class="mb-1">
-            <a
-              class="block p-4 text-sm font-semibold text-black hover:text-accent"
-              href="find-an-agent.html"
-              >Find an agent</a
-            >
-          </li>
-          <li class="mb-1">
-            <a
-              class="block p-4 text-sm font-semibold text-black hover:text-accent"
-              href="gallery.html"
-              >Gallery</a
-            >
-          </li>
-          <li class="mb-1">
-            <a
-              class="block p-4 text-sm font-semibold text-black hover:text-accent"
-              href="contact-us.html"
-              >Contact us</a
-            >
-          </li>
-        </ul>
-      </div>
-
-      <!-- Mobile Menu Toggle Button -->
-      <div class="lg:hidden">
-        <button class="navbar-burger flex items-center text-[#ffdd00] p-3">
-          <svg
-            class="block h-6 w-6 fill-current"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    <div class="hidden lg:block">
+      <ul class="flex">
+        <li class="mb-1">
+          <a
+            class="block p-4 text-sm font-semibold text-black hover:text-accent rounded"
+            href="#"
+            >Home</a
           >
-            <title>Mobile menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-          </svg>
-        </button>
-      </div>
-    </nav>
-  </div>
-
-  <!-- Mobile Menu Section -->
+        </li>
+        <li class="mb-1">
+          <a
+            class="block p-4 text-sm font-semibold text-black hover:text-accent  rounded"
+            href="about-us-page.html"
+            >About Us</a
+          >
+        </li>
+        <li class="mb-1">
+          <a
+            class="block p-4 text-sm font-semibold text-black hover:text-accent  rounded"
+            href="become-an-agent.html"
+            >Become an Agent</a
+          >
+        </li>
+        <li class="mb-1">
+          <a
+            class="block p-4 text-sm font-semibold text-black hover:text-accent  rounded"
+            href="agent-list.html"
+            >Find an Agent</a
+          >
+        </li>
+        <li class="mb-1">
+          <a
+            class="block p-4 text-sm font-semibold text-black hover:text-accent  rounded"
+            href="#gallery"
+            >Gallery</a
+          >
+        </li>
+        <li class="mb-1">
+          <a
+            class="block p-4 text-sm font-semibold text-black hover:text-accent  rounded"
+            href="contact-us.html"
+            >Contact</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="lg:hidden block">
+      <button class="navbar-burger flex items-center text-[#ffdd00] p-3">
+        <svg
+          class="block h-6 w-6 fill-current"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Mobile menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+        </svg>
+      </button>
+    </div>
+  </nav>
+  <!---------- bottom-nav ---------->
   <div class="navbar-menu relative z-50 hidden">
-    <!-- Overlay Background -->
     <div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
-
-    <!-- Mobile Navigation Panel -->
     <nav
       class="fixed top-0 right-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto"
     >
-      <!-- Mobile Header Section -->
       <div class="flex items-center mb-8">
         <a
           rel="noopener noreferrer"
@@ -163,10 +132,9 @@
           <img
             src="./images/logo/hulas-remittance-logo.jpg"
             class="w-40"
-            alt="Hulas Remittance Mobile Logo"
+            alt=""
           />
         </a>
-
         <button class="navbar-close">
           <svg
             class="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500"
@@ -180,18 +148,16 @@
               stroke-linejoin="round"
               stroke-width="2"
               d="M6 18L18 6M6 6l12 12"
-            />
+            ></path>
           </svg>
         </button>
       </div>
-
-      <!-- Mobile Navigation Links -->
       <div>
         <ul>
           <li class="mb-1">
             <a
               rel="noopener noreferrer"
-              class="block p-4 text-sm font-semibold text-gray-400 hover:text-accent"
+              class="block p-4 text-sm font-semibold text-gray-400 hover:text-accent rounded"
               href="index.html"
               >Home</a
             >
@@ -199,24 +165,25 @@
           <li class="mb-1">
             <a
               rel="noopener noreferrer"
-              class="block p-4 text-sm font-semibold text-gray-400 hover:text-accent"
-              href="about-us.html"
+              class="block p-4 text-sm font-semibold text-gray-400 hover:text-accent rounded"
+              href="#about_us"
               >About Us</a
             >
           </li>
+
           <li class="mb-1">
             <a
               rel="noopener noreferrer"
-              class="block p-4 text-sm font-semibold text-gray-400 hover:text-accent"
-              href="home-become-an-agent.html"
+              class="block p-4 text-sm font-semibold text-gray-400 hover:text-accent rounded"
+              href="#become_agent"
               >Become an Agent</a
             >
           </li>
           <li class="mb-1">
             <a
               rel="noopener noreferrer"
-              class="block p-4 text-sm font-semibold text-gray-400 hover:text-accent"
-              href="gallery.html"
+              class="block p-4 text-sm font-semibold text-gray-400 hover:text-accent rounded"
+              href="#gallery"
               >Gallery</a
             >
           </li>
@@ -224,18 +191,36 @@
             <a
               rel="noopener noreferrer"
               class="block p-4 text-sm font-semibold text-gray-400 hover:text-accent rounded"
-              href="contact-us.html"
+              href="#contact_p"
               >Contact</a
             >
           </li>
+          <li class="mb-1">
+            <a
+              rel="noopener noreferrer"
+              href="https://www.westernunion.com/us/en/send-money/app/price-estimator"
+              target="_blank"
+              class="block p-4 text-sm font-semibold text-accent rounded"
+            >
+              Our Fees
+            </a>
+          </li>
+          <li class="mb-1">
+            <a
+              rel="noopener noreferrer"
+              href="https://www.westernunion.com/us/en/send-money-to-nepal.html"
+              target="_blank"
+              class="block p-4 text-sm font-semibold text-accent rounded"
+              >Forex Rates
+            </a>
+          </li>
         </ul>
       </div>
-
-      <!-- Western Union Logo Footer -->
       <div class="mt-auto">
         <div class="flex justify-end">
           <img
-            src="./images/logo/WesternUnion_HorizontalLockup_YellowBlack.png"
+            {{-- src="./images/logo/WesternUnion_HorizontalLockup_YellowBlack.png" --}}
+            src="{{ asset('assets/images/logo/WesternUnion_HorizontalLockup_YellowBlack.png') }}"
             class="w-44 h-5"
             alt="Western Union Logo"
           />

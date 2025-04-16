f8fd085278b63aa5c3a5c1ca46c1c6de54dee0da
Silika <silicakoirala@gmail.com>
2025-04-16 11:26:12 +0545

    Add Text and Button to slider

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -14,13 +14,31 @@
         <section id="home-slider">
             <div id="default-carousel" class="relative w-full" data-carousel="slide">
                 <!-- Carousel wrapper -->
-                <div class="relative h-80 overflow-hidden rounded-lg md:h-[600px] ">
+                <div class="relative h-[200px] sm:h-[300px] overflow-hidden rounded-lg md:h-[400px] lg:h-[500px]">
                     <!-- Item 1 -->
                     @foreach ($sliders as $key => $slider)
                         <div class="hidden duration-700 ease-in-out" data-carousel-item="{{ $key === 0 ? 'active' : '' }}">
                             <img src="{{ asset('storage/' . $slider->image) }}"
                                 class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                                 alt="{{ $slider->title ?? 'Slider Image' }}" />
+
+                            <!-- Text Overlay -->
+                            <div
+                                class="absolute inset-0 flex flex-col space-y-3 items-left text-left bg-black/10 py-10 sm:py-20 md:py-40 px-20 md:pb-50 md:px-30">
+                                <h2 class="text-white text-xl sm:text-3xl md:text-5xl font-bold"      style="
+                                  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.01);
+                                  -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
+                                  ">Reliable Services</h2>
+                                <p class=" text-white text-lg sm:text-xl md:text-2xl max-w-3xl"      style="
+                                      -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
+                                  ">Experience secure and
+                                    hassle-free money transfers with Hulas Remittance — trusted by millions across Nepal since 2005.
+                                </p>
+                                <a href="{{ route('aboutHulasRemittance') }}"
+                                    class="px-6 py-2 bg-accent w-40 text-black border-2 rounded-full hover:opacity-85 font-semibold text-lg"
+                               >
+                                    About Hulas </a>
+                            </div>
                         </div>
                     @endforeach
                 </div>
@@ -71,11 +89,10 @@
     <section class="overflow-x-hidden">
         <div class="p-4 md:ml-8 lg:my-4 lg:mx-20 lg:mb-2">
             <div class="flex flex-col items-center space-y-6">
-                <h1 class="font-bold text-accent uppercase text-lg tracking-wider"
-                    style="
-      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.01);
-      -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
-      ">
+                <h1 class="font-bold text-accent uppercase text-lg tracking-wider" style="
+                          text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.01);
+                          -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
+                          ">
                     Our Introduction
                 </h1>
 
@@ -122,11 +139,10 @@
             <section class="overflow-x-hidden">
                 <div class="p-4 md:ml-8 lg:my-4 lg:mx-20 lg:mb-2">
                     <div class="flex flex-col items-center space-y-6">
-                        <h1 class="font-bold text-accent uppercase text-lg tracking-wider"
-                            style="
-      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.01);
-      -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
-      ">
+                        <h1 class="font-bold text-accent uppercase text-lg tracking-wider" style="
+                                              text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.01);
+                                              -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
+                                              ">
                             Our Services
                         </h1>
 
@@ -264,11 +280,10 @@
             <div class="overflow-x-hidden">
                 <div class="p-4 md:ml-8 lg:my-4 lg:mx-20 lg:mb-2">
                     <div class="flex flex-col items-center space-y-6">
-                        <h1 class="font-bold text-[#FDDC2B] uppercase text-lg tracking-wider"
-                            style="
-      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.01);
-      -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
-      ">
+                        <h1 class="font-bold text-[#FDDC2B] uppercase text-lg tracking-wider" style="
+                                              text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.01);
+                                              -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
+                                              ">
                             Our partners & Supporters
                         </h1>
 
@@ -285,31 +300,28 @@
 
             <div class="w-full overflow-hidden relative">
                 <div class="w-full h-full absolute">
-                    <div class="w-1/4 h-full absolute z-50 left-0"
-                        style="
-      background: linear-gradient(
-      to right,
-      #fff8cc 0%,
-      rgba(255, 255, 255, 0) 100%
-      );
-      ">
+                    <div class="w-1/4 h-full absolute z-50 left-0" style="
+                                              background: linear-gradient(
+                                              to right,
+                                              #fff8cc 0%,
+                                              rgba(255, 255, 255, 0) 100%
+                                              );
+                                              ">
                     </div>
-                    <div class="w-1/4 h-full absolute z-50 right-0"
-                        style="
-      background: linear-gradient(
-      to left,
-      #fff8cc 0%,
-      rgba(255, 255, 255, 0) 100%
-      );
-      ">
+                    <div class="w-1/4 h-full absolute z-50 right-0" style="
+                                              background: linear-gradient(
+                                              to left,
+                                              #fff8cc 0%,
+                                              rgba(255, 255, 255, 0) 100%
+                                              );
+                                              ">
                     </div>
                 </div>
 
-                <div class="carousel-items flex items-center justify-center w-full"
-                    style="
-      width: fit-content;
-      animation: carouselAnim 10s infinite alternate linear;
-      ">
+                <div class="carousel-items flex items-center justify-center w-full" style="
+                                              width: fit-content;
+                                              animation: carouselAnim 10s infinite alternate linear;
+                                              ">
                     @foreach ($partners as $partner)
                         <div class="carousel-focus flex items-center flex-col relative bg-white mx-5 my-10 px-4 py-3"
                             style="width: 270px">
@@ -383,8 +395,7 @@
                         <div class="mx-3">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"
                                 fill="currentColor">
-                                <path d="M3 6h14M3 12h14M3 18h14" stroke="black" stroke-width="2"
-                                    stroke-linecap="round" />
+                                <path d="M3 6h14M3 12h14M3 18h14" stroke="black" stroke-width="2" stroke-linecap="round" />
                                 <circle cx="20" cy="6" r="1.5" fill="black" />
                                 <circle cx="20" cy="12" r="1.5" fill="black" />
                                 <circle cx="20" cy="18" r="1.5" fill="black" />
@@ -399,8 +410,7 @@
                             <div class="flex flex-row gap-10 p-2 border-l-accent border-l-[4px] my-2 shadow-sm h-25">
                                 <div class="h-auto w-30">
                                     <img src="{{ isset($news->image) ? asset('storage/' . $news->image) : asset('assets/images/placeholder.jpg') }}"
-                                        alt="{{ $news->title_en ?? 'News Image' }}"
-                                        class="h-full w-full rounded-lg object-cover" />
+                                        alt="{{ $news->title_en ?? 'News Image' }}" class="h-full w-full rounded-lg object-cover" />
                                 </div>
 
                                 <div class="flex flex-col gap-3">
@@ -526,7 +536,7 @@
     </script>
 
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             const buttons = document.querySelectorAll(".modal-button");
 
             buttons.forEach((button) => {

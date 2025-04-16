cd0acd5804c683b0294ca2c381e3c2dba55749a6
Birajwagley <birazwagley8739@gmail.com>
2025-04-16 13:13:22 +0545

    Slider Content

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -22,16 +22,6 @@
 
                             <!-- Text Overlay -->
                             <div
-<<<<<<< HEAD
-                                class="absolute inset-0 flex flex-col space-y-3 items-left text-left bg-black/10 py-10 sm:py-20 md:py-40 px-20 md:pb-50 md:px-30">
-                                <h2 class="text-white text-xl sm:text-3xl md:text-5xl font-bold"      style="
-                                  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.01);
-                                  -webkit-text-stroke: 1px rgba(19, 18, 18, 0.24);
-                                  ">Reliable Services</h2>
-                                <p class=" text-white text-lg sm:text-xl md:text-2xl max-w-3xl"      style="
-                                      -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
-                                  ">Experience secure and
-=======
                                 class="absolute inset-0 flex flex-col space-y-3 items-left text-left bg-black/20 py-10 sm:py-20 md:py-40 px-20 md:pb-50 md:px-30">
                                 <h2 class="text-white text-xl sm:text-3xl md:text-5xl font-bold" style="
                                               text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.01);
@@ -40,7 +30,6 @@
                                 <p class=" text-white text-lg sm:text-xl md:text-2xl max-w-3xl" style="
                                                   -webkit-text-stroke: 1px rgba(19, 18, 18, 0.096);
                                               ">Experience secure and
->>>>>>> cba7a885b04a23bb6f4427e7ecc99e5d1403ccc3
                                     hassle-free money transfers with Hulas Remittance — trusted by millions across Nepal since 2005.
                                 </p>
                                 <a href="{{ route('aboutHulasRemittance') }}"

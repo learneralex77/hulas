68cc2be94bf3af28ef85cc5fe397b5c70072afb8
Silika <silicakoirala@gmail.com>
2025-04-17 15:12:35 +0545

    Update homepage.blade.php

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -404,7 +404,7 @@
                     Explore News Articles
                 </a>
             </div>
-            <div class="drop-shadow-xl shadow-gray-100 bg-white rounded-lg m-3">
+            <div class="drop-shadow-xl shadow-gray-100 bg-white rounded-lg">
                 <!-- Heading for scroll -->
                 <div class="flex flex-row bg-white rounded m-3 p-3">
                     <!-- Explore part -->
@@ -474,7 +474,7 @@
 
                     <!-- <img src="{{ asset('assets/images/agent/agent.jpg') }}" alt="Modal Image" srcset=""
                                                     class="mx-auto w-full h-full" /> -->
-                    <div class="swiper !h-[60vh] mx-auto w-[100%] popupSwiper rounded-lg">
+                    <div class="swiper !h-[40vh] md:!h-[60vh] mx-auto w-[100%] popupSwiper rounded-lg">
                         <div class="swiper-wrapper">
                             <div class="swiper-slide">
                                 <a href="homepage">
